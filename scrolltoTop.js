b8421c9bf51b5ffa8d043a44f0243c9863a9c364
Sandeep Biswal <samdeepbiswal123@gmail.com>
2023-05-24 17:59:10 +0530

    scroll to top button added

---
+++ scrolltoTop.js
@@ -0,0 +1,23 @@
+// Get the "return to top" button element
+var returnToTopButton = document.getElementById('returnToTop');
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Function to toggle the visibility of the "return to top" button
+function toggleReturnToTopButton() {
+  if (window.scrollY > 200) {
+    returnToTopButton.classList.add('active');
+  } else {
+    returnToTopButton.classList.remove('active');
+  }
+}
+
+// Add event listeners
+window.addEventListener('scroll', toggleReturnToTopButton);
+returnToTopButton.addEventListener('click', scrollToTop);
