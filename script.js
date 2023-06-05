9549caa8a31298db463a1b2278cbddbefc8c40fa
ankitjt <48979824+ankitjt@users.noreply.github.com>
2023-06-05 14:43:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,34 +6,32 @@ function googleTranslateElementInit ()
   )
 }
 
-let openDropDown = document.querySelector( '.openDropDown' ),
-  dropMenuLinks = document.querySelector( '.dropMenuLinks' ),
-  contentWrapper = document.querySelector( '.contentWrapper' ),
-  navbar = document.querySelector( '.navbar' ),
+let dropMenuLinks = document.querySelector( '.dropMenuLinks' ),
+  indexOpenDropDown = document.querySelector( '.indexOpenDropDown' ),
+  indexNavbar = document.querySelector( '.indexNavbar' ),
   cardAr = document.querySelector( '.cardAr' ),
   cardIskon = document.querySelector( '.cardIskon' ),
   cardStory = document.querySelector( '.cardStory' ),
   cardClock = document.querySelector( '.cardClock' ),
   cardShlok = document.querySelector( '.cardShlok' ),
+  contentWrapper = document.querySelector( '.contentWrapper' ),
   cardRejuvenate = document.querySelector( '.cardRejuvenate' )
 
 
 contentWrapper.onscroll = () =>
 {
-  contentWrapper.scrollTop > 5 ? navbar.classList.add( 'bg-white' ) : navbar.classList.add( 'transparent' )
+  contentWrapper.scrollTop > 5 ? indexNavbar.classList.add( 'bg-white', 'border-b', 'border-b-gray-900' ) : indexNavbar.classList.remove( 'bg-white', 'border-b', 'border-b-gray-900' )
   contentWrapper.scrollTop > 1100 ? cardAr.classList.add( 'right-0' ) : ''
   contentWrapper.scrollTop > 1600 ? cardIskon.classList.add( 'left-0' ) : ''
   contentWrapper.scrollTop > 2300 ? cardStory.classList.add( 'right-0' ) : ''
   contentWrapper.scrollTop > 3100 ? cardClock.classList.add( 'left-0' ) : ''
   contentWrapper.scrollTop > 3850 ? cardShlok.classList.add( 'right-0' ) : ''
   contentWrapper.scrollTop > 4500 ? cardRejuvenate.classList.add( 'left-0' ) : ''
-
 }
 
-openDropDown.onclick = () =>
+indexOpenDropDown.onclick = () =>
 {
   dropMenuLinks.style.right = 0
-
 }
 
 let closeDropDown = document.querySelector( ".closeDropDown" )
@@ -41,8 +39,6 @@ closeDropDown.onclick = () =>
 {
   dropMenuLinks.style.right = '-2000px'
 }
-
-
 
 // Ends all pages menu interaction 
 
