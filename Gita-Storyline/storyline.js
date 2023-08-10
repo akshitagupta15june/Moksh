7200a4a2255d69a98077da2ef0b200e7cae850b7
debdutta04 <debdutta0401@gmail.com>
2023-08-10 22:24:02 +0530

    add a play pause button

--- Gita-Storyline/storyline.js
+++ Gita-Storyline/storyline.js
@@ -88,3 +88,17 @@ right.addEventListener("click", () => {
 });
 
 showQuestion();
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play')
+  var soundOff = document.getElementById('pause')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+} 
