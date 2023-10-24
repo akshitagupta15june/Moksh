539bb6fbc60e90dcda5530c9e13fedb1193584f2
dwivedishrey <97790062+dwivedishrey@users.noreply.github.com>
2023-10-24 18:14:20 +0530

    darkmode feature added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let dropMenuLinks = document.querySelector('.dropMenuLinks');
 let indexOpenDropDown = document.querySelector('.indexOpenDropDown');
 let closeDropDown = document.querySelector('.closeDropDown');
+var icon=document.getElementById("icon");
+icon.onclick=function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src="sun.png";
+  }
+  else{
+    icon.src="moon.png"
+  }
+}
 
 indexOpenDropDown.onclick = () => {
   dropMenuLinks.style.right = 0;
